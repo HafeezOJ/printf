5a5637885427092a99978ed53153d8bd3448111c
HafeezOJ <ho.adepoju@gmail.com>
2022-11-24 21:36:45 +0000

    add missing headers

--- main.h
+++ main.h
@@ -5,8 +5,10 @@
 #include <stdarg.h>
 #include <stdlib.h>
 
+int ev_print_func(const char *s, int index);
+int (*get_print_func(const char *s, int index))(va_list, char *, unsigned int);
 unsigned int handl_buf(char *buf, char c, unsigned int ibuf);
-int print_buf(char *buf, unsigned int nbuf);`
+int print_buf(char *buf, unsigned int nbuf);
 int _printf(const char *format, ...);
 
 #endif
